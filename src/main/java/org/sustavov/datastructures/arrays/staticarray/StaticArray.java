432493ec7255de7e720914c436faa78ebf26d0b5
Anton Sustavov <antsustavov@gmail.com>
2021-03-07 21:02:26 +0200

    first commit

---
+++ src/main/java/org/sustavov/datastructures/arrays/staticarray/StaticArray.java
@@ -0,0 +1,12 @@
+package org.sustavov.datastructures.arrays.staticarray;
+
+/**
+ * @author Anton Sustavov
+ * @since 2021/03/07
+ */
+public class StaticArray {
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        System.out.println(array.length);
+    }
+}
